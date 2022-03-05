896bcad4e766e031e9422e1a330a862998b006ef
Silas <silas23.fr@gmail.com>
2022-03-05 10:53:08 -0300

    feat: add post and get routes to customers table

--- src/Controllers/customerControler.js
+++ src/Controllers/customerControler.js
@@ -1,9 +1,34 @@
 import connection from "../database.js";
 
 export async function customerPost(req, res, next) {
-  const customer = res.locals.customer;
+  const { name, phone, cpf, birthday } = res.locals.customer;
   try {
-    console.log(customer);
+    await connection.query(`
+        INSERT 
+        INTO customers (name, phone, cpf, birthday) 
+        VALUES ('${name}', '${phone}', '${cpf}', '${birthday}')
+    `);
+    res.sendStatus(201);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+}
+
+export async function getCustomers(req, res, next) {
+  const { id } = req.params;
+  try {
+    let idFilter = "";
+    if (id) {
+      idFilter = `WHERE id=${id} LIMIT 1`;
+    }
+    const result = await connection.query(`
+        SELECT * 
+        FROM customers
+        ${idFilter}
+      `);
+
+    res.send(result.rows);
   } catch (e) {
     console.log(e);
     res.sendStatus(500);
--- src/Routes/customerRouter.js
+++ src/Routes/customerRouter.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { customerPost } from "../Controllers/customerControler.js";
+import {
+  customerPost,
+  getCustomers,
+} from "../Controllers/customerControler.js";
 import { validateCustomerPost } from "../middlewares/customerMiddleware.js";
 
 const customerRouter = Router();
 
 customerRouter.post("/customers", validateCustomerPost, customerPost);
+customerRouter.get("/customers", getCustomers);
+customerRouter.get("/customers/:id", getCustomers);
 
 export default customerRouter;
